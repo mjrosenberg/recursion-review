dcc37c8646cc96907165186706fb7155be1cb26c
mjrosenberg <mjr70@duke.edu>
2020-05-11 15:16:35 -0700

    fixing cosmetic errors

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,47 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+// your code goes here
+  if (typeof obj === "string"){
+    return '"' + obj + '"';
+  }  else if (typeof obj === "number"){
+    return String(obj);
+  }  else if (typeof obj === "boolean"){
+    return String(obj);
+  }  else if(Array.isArray(obj) === true){
+    var ans = "[";
+    if (obj[0] === undefined){
+      return "[]";
+    }
+    for (var i = 0; i< obj.length; i++){
+      ans = ans + stringifyJSON(obj[i]) + ",";
+      //first recursive call
+    }
+    ans = ans.substring(0, ans.length -1);
+    ans += "]";
+    return ans;
+  } else if (obj === null){
+    return "null";
+  } else if (obj instanceof Object) {
+    var arrayOfKeyValues = [];
+
+    objKeys = Object.keys(obj);
+
+    objKeys.forEach(function(key) {
+      var keyOut = '"' + key + '":';
+      var keyValOut = obj[key];
+      if (keyValOut instanceof Function || typeof keyValOut === undefined){
+        arrayOfKeyValues.push('');
+      } else if (typeof keyValOut === 'string'){
+        arrayOfKeyValues.push(keyOut + '"' + keyValOut + '"');
+      } else if (typeof keyValOut === 'boolean' || typeof keValOut === 'number' || keyValOut === null){
+        arrayOfKeyValues.push(keyOut + keyValOut);
+      } else if (keyValOut instanceof Object) {
+        arrayOfKeyValues.push(keyOut + stringifyJSON(keyValOut));
+      }
+    });
+    return '{' + arrayOfKeyValues + '}';
+  }
 };
+// https://github.com/elmarek/ghrphx10-recursion-review.git
+
